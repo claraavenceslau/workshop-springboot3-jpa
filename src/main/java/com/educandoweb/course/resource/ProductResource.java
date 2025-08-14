f709d2607d342550e2e999fbdc6660a62de11432
Maria <mclaravenceslaum@gmail.com>
2025-08-13 22:26:22 -0300

    Product

---
+++ src/main/java/com/educandoweb/course/resource/ProductResource.java
@@ -0,0 +1,32 @@
+package com.educandoweb.course.resource;
+
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAllProducts() {
+        List<Product> list = productService.findAllProducts();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id) {
+        Product obj = productService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
