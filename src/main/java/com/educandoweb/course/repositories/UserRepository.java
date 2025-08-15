eafb6be7ebc72488dd53476a2210ec44a1dfc860
Maria <mclaravenceslaum@gmail.com>
2025-08-14 22:19:14 -0300

    User update

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -4,4 +4,5 @@ import com.educandoweb.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Long id(Long id);
 }
